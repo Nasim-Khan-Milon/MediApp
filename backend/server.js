fb9ac41c6924cff98850f3f6af1d025cc28de3ff
Nasim-Khan_Milon <nasmkhanmilon@gmail.com>
2025-12-01 21:57:23 +0600

    Basic setup for my project

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,13 @@
 import express from 'express'
 import cors from 'cors'
 import 'dotenv/config'
+import db from './config/db.js'
+import connectCloudinary from './config/cloudinary.js'
 
 //app config
 const app = express()
 const port = process.env.PORT || 4000
+connectCloudinary()
 
 //middleware
 app.use(express.json())
